0e61e40c0ecf0c23f7ee32f80c7cfcadb59c5b7f
MashaOsipova <mashaosi395@mail.ru>
2022-12-11 10:32:18 +0300

    testng

---
+++ src/main/java/ru/netology/unit/Main.java
@@ -0,0 +1,9 @@
+package ru.netology.unit;
+
+public class Main {
+    public static void main(String[] args) {
+        CashbackHackService service = new CashbackHackService();
+
+        System.out.println("Вам нужно потратить ещё " + service.remain(10) + " чтобы получить кэшбек");
+    }
+}
